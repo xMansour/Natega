bfc0d6bfc82f4d6fff4e37fbc75e6ae3d353cc4c
xMansour <melsayedmansour96@gmail.com>
2019-04-12 21:04:55 +0200

    UI changes and Arabic support.

--- Application/app/src/main/java/com/mansourappdevelopment/androidapp/natega/activities/MainActivity.java
+++ Application/app/src/main/java/com/mansourappdevelopment/androidapp/natega/activities/MainActivity.java
@@ -1,10 +1,12 @@
 package com.mansourappdevelopment.androidapp.natega.activities;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -30,15 +32,14 @@ public class MainActivity extends AppCompatActivity {
         txtID = (EditText) findViewById(R.id.txtID);
         btnSendMessage = (Button) findViewById(R.id.btnSendMessage);
 
-        requestPermission();
 
         btnSendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //requestPermission();
+                requestPermission();
                 String academicNumber = txtID.getText().toString();
                 if (academicNumber.equals("") || academicNumber.length() != 7)
-                    txtID.setError("Enter a valid academic number");
+                    txtID.setError(getResources().getString(R.string.edit_text_error));
                 else
                     sendMessage(academicNumber);
             }
@@ -51,10 +52,10 @@ public class MainActivity extends AppCompatActivity {
         try {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(SERVER_NUMBER, null, academicNumber, null, null);
-            Toast.makeText(this, "Message sent", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getResources().getString(R.string.message_sent), Toast.LENGTH_SHORT).show();
         } catch (SecurityException e) {
             //Toast.makeText(this, "Permission needed", Toast.LENGTH_SHORT).show();
-            requestPermission();
+            //requestPermission();
         }
 
     }
@@ -62,17 +63,41 @@ public class MainActivity extends AppCompatActivity {
     private void requestPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, REQUEST_CODE);
+                new AlertDialog.Builder(this)
+                        .setTitle(getResources().getString(R.string.permission_needed))
+                        .setMessage(getResources().getString(R.string.permission_explanation))
+                        .setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, REQUEST_CODE);
+                            }
+                        })
+                        .setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create().show();
 
             } else {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, REQUEST_CODE);
 
             }
-        } else {
-
         }
 
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, getResources().getString(R.string.permission_granted), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, getResources().getString(R.string.permission_denied), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
